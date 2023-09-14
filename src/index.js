91483651c32130971a93c452f1a89fe3203161f3
igrootdev <ander.silva2710@gmail.com>
2023-09-14 19:03:40 -0300

    Instalando Pacotes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+require('dotenv').config();
 
+const express = require('express');
+const rotas = require('./router');
+const app = express();
+
+app.use(express.json());
+app.use(rotas);
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Servidor rodando na porta ${process.env.SERVER_PORT}`);
+});
